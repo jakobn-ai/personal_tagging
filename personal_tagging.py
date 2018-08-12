af5f66b0ccac5e06e922b90d4df11182bb82f4a0
Jakob-Naucke <jakob.naucke@web.de>
2018-08-12 16:13:28 +0200

    Added getting year

--- personal_tagging.py
+++ personal_tagging.py
@@ -5,6 +5,7 @@
  """
 
 import musicbrainzngs
+import re
 
 # Developing only, but I do not want to look it up every time I need it
 # import pprint
@@ -21,26 +22,32 @@ def get_artist_id(name):
     raise ValueError("Artist %s not literally found" % name)
 
 
-def get_album_id(name, artist_id):
-    """Find the ID of an album by its name and its artist ID"""
+def get_album_id(name, artist_id, artist_name):
+    """Find the ID of an album by its name and its artist ID.
+    Artist name is for error output
+    """
     albums_dict = musicbrainzngs.search_releases(query=name, arid=artist_id)
     for album in albums_dict["release-list"]:
         if name == album["title"]:
             return album["id"]
-    raise ValueError("Album %s not literally found from artist %s" % (
-        name, musicbrainzngs.get_artist_by_id(artist_id)["artist"]["name"]))
+    raise ValueError("Album %s not literally found from artist %s" %
+                     (name, artist_name))
 
 
-def get_song_list(album_id):
-    """Find the songs of an album by its ID"""
+def get_song_information(album_id):
+    """Find the songs and the release year of an album by its ID"""
     album_dict = musicbrainzngs.get_release_by_id(id=album_id,
                                                   includes="recordings")
+    output_dict = {}
+    output_dict["year"] = re.sub(r"([0-9]{4})(-[0-9]{2}){2}",
+                                 r"\1",
+                                 album_dict["release"]["date"])
     result_track_list = album_dict["release"]["medium-list"][0]["track-list"]
     sorted(result_track_list, key=lambda song: song["position"])
-    output_track_list = []
+    output_dict["tracks"] = []
     for song in result_track_list:
-        output_track_list.append(song["recording"]["title"])
-    return output_track_list
+        output_dict["tracks"].append(song["recording"]["title"])
+    return output_dict
 
 
 def main():
